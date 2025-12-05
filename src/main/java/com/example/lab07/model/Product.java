d20c3a1fb0328daadedcebf36a57892d8de3e2c1
Alexandra <smir0002@hz.nl>
2025-12-05 14:16:17 +0100

    first commit + product model created

---
+++ src/main/java/com/example/lab07/model/Product.java
@@ -0,0 +1,48 @@
+package com.example.lab07.model;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String category;
+    private Double price;
+    private String description;
+
+    public Product() {}
+
+    // constructor with all mandatory fields
+    public Product(Long id, String name, String category, Double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    // get info and set info
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public Double getPrice() { return price; }
+    public void setPrice(Double price) { this.price = price; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
